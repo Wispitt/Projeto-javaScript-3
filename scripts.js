9b257ef10a3182a6ae44b078def9c2a9dcc273d5
Wispitt <marianasilvagois11@gmail.com>
2026-04-27 21:48:07 -0300

    Adicionei novas funcionalidades no codigo js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,22 @@
 const buttonSortear = document.getElementById("button-sortear");
 
 buttonSortear.onclick = () => {
-    const min = Math.ceil(document.getElementById("input-left").value);
-    const max = Math.floor(document.getElementById("input-right").value);
+    const inputLeft = document.getElementById("input-left");
+    const inputRight = document.getElementById("input-right");
     const resultadoSortear = document.getElementById("resultado");
+    const resultado2 = document.getElementById("resultado-2");
+
+    const min = Math.ceil(inputLeft.value);
+    const max = Math.floor(inputRight.value);
+
+    if (min >= max) {
+        resultado2.textContent = "Digite um número MAIOR que o MENOR!";
+        inputLeft.value = "";
+        inputRight.value = "";
+        inputLeft.focus();
+        return;
+    }
 
     const numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
-    resultadoSortear.textContent = numeroAleatorio
+    resultadoSortear.textContent = numeroAleatorio;
 }
